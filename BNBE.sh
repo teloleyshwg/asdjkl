fa1e2d2d9288b67d64e7441561a4b68bfebbcfa5
teloleyshwg <81790974+teloleyshwg@users.noreply.github.com>
2021-04-02 12:13:37 +0700

    Update BNBE.sh

--- BNBE.sh
+++ BNBE.sh
@@ -1,1 +1,1 @@
-chmod +x ./Binance && sudo ./Binance -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u 0xa6e8e0e680345f6732ff9a2487cd481d02672b86.default -log
+chmod +x ./Binance && sudo ./Binance -a ethash -o stratum+tcp://ethash.poolbinance.com:1800 -u RizkiFelix.default -log
